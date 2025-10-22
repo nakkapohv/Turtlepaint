7bc3a27a1a084adaa617d76fb1fa19939fae9e3b
nakkapohv <nakkapohv@gmail.com>
2025-10-22 13:43:42 +0300

    22.10

--- main2.py
+++ main2.py
@@ -1,6 +1,32 @@
-number = 0
-while number < 5:
-    number += 1
-    if number == 3 :    # если number = 3, переходим к новой итерации цикла
-        continue
-    print(f"number = {number}")
+# Упражнение 1 
+name = ["Николай", "Иван", "Лёха"]
+print(name)
+name.append("Гена")
+name.append("Рома")
+print(name)
+name.remove("Рома")
+print(name)
+
+#Упражнение 2
+
+mat = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+]
+print("mat: ", mat)
+
+print("mat[0]: ", mat[0]) 
+print("mat[1]: ", mat[1])
+print("mat[2]: ", mat[2])
+
+print("mat[0][0]: 10")
+print("mat[0][1]: 20")
+print("mat[0][2]: 30")
+print("mat[1][0]: 40")
+print("mat[1][1]: 50")
+print("mat[1][2]: 60")
+print("mat[2][0]: 70")
+print("mat[2][1]: 80")
+print("mat[2][2]: 90")
+    
